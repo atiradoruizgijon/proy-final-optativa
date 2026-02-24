833a75e3175e723cf89d3cdccf27aaf44d70454f
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-02-24 19:00:46 +0100

    prueba foreach angular

--- FrontEnd/kook/src/app/recetas/recetas.ts
+++ FrontEnd/kook/src/app/recetas/recetas.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './recetas.css',
 })
 export class Recetas {
-
+  recetas = [
+    {
+      titulo: "Espaguetis a la carbonara",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? NSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adam, id. Corporis necessitatibus earum vero possimus ad."
+    },
+    {
+      titulo: "Espaguetis con tomate",
+      descripcion: "Lorem ipsum dolor sit ameSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adt consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad."
+    },
+    {
+      titulo: "Espaguetis a la carbonara",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad."
+    },
+    {
+      titulo: "Espaguetis con tomate",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad."
+    },
+    {
+      titulo: "Espaguetis a la carbonara",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad.Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus adSunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad"
+    },
+    {
+      titulo: "Espaguetis con tomate",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad."
+    },
+    {
+      titulo: "Espaguetis a la carbonara",
+      descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt adipisci quae quas? Nam, id. Corporis necessitatibus earum vero possimus ad."
+    }
+  ];
 }
